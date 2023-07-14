240ab037cd05efe0531b58d7ca7710b93126eb63
kokolopo <kokolopo321@gmail.com>
2023-07-14 11:22:32 +0700

    a

--- src/utils/time_format.js
+++ src/utils/time_format.js
@@ -1,4 +1,4 @@
-const ubahFormatWaktu = (waktu) => {
+const ubahFormatWaktu = (waktu = '03:00:00') => {
     const waktuArr = waktu.split(':')
     const jam = waktuArr[0]
     const menit = waktuArr[1]
